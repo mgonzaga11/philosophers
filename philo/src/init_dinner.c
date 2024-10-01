a9af1bd1bbfe62c03f3491f0c9b57078569a2436
Marcela Gonzaga <mgonzaga@c1r4p3.42sp.org.br>
2024-10-01 17:18:04 -0300

    quase

---
+++ philo/src/init_dinner.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_dinner.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgonzaga <mgonzaga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 13:35:22 by mgonzaga          #+#    #+#             */
+/*   Updated: 2024/10/01 17:16:13 by mgonzaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	unic_philo(t_philo *s_philo)
+{
+	global_print(get_actual_time(), s_philo, "has taken a fork");
+	usleep(s_philo->time_to_die * 1000);
+	global_print(get_actual_time(), s_philo, "died");
+}
+
+void	init_dinner(t_data s_data, t_philo *s_philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < s_data.philos)
+	{
+		pthread_create(&s_philo[i].thread, NULL, dinner, &s_philo[i]);
+		i++;
+	}
+	i = 0;
+	while(!check_philo_death(&s_philo[i]))
+		i = (i + 1) % s_data.philos;
+	i = 0;
+	while (i < s_data.philos)
+	{
+		pthread_join(s_philo[i].thread, NULL);
+		i++;
+	}
+}
+
+void	*dinner(void *p_param)
+{
+	t_philo	*s_philo;
+	int	i;
+	int a;
+
+	a = 0;
+	i = 0;
+	s_philo = (t_philo *)p_param;
+
+	while (!check(s_philo->s_data))
+	{
+		if ( i >= s_philo->s_data->times_must_eat)
+			break ;
+		i = i + philo_eat(s_philo);
+		sleeping(s_philo);
+		thinking(s_philo);
+	}
+	return (NULL);
+}
